ce9a053ad60609e880ac0de59305f088786df373
NicWille <nic.wille1020@yahoo.com>
2022-04-03 15:40:17 -0700

    created qrcode

--- qrcode.ts
+++ qrcode.ts
@@ -1,1 +1,3 @@
-console.log("hello world")
+import {qrcode} from "https://deno.land/x/qrcode@v2.0.0/mod.ts"
+const fixedSizeImage = await qrcode("https://refactoring.guru/design-patterns", { size: 400 });
+Deno.writeTextFile("qrcode.html", `<img src="${fixedSizeImage}"/>`)
